345da2dacfec8514e36fb157f9a8d3367b4f15a0
Cyril Lopez <cyrlop06@hotmail.com>
2020-08-06 14:21:51 +0000

    Reformatted add_process_event_to_dataset

--- metadata_registration_lib/study_upload.py
+++ metadata_registration_lib/study_upload.py
@@ -118,90 +118,60 @@ def add_dataset_to_study(dataset_data, study_id, host, email=None, password=None
     return dataset_uuid
 
 
-# def add_process_event_to_dataset(pe_data, study_id, dataset_uuid, host, email=None, password=None):
-#     """
-#     The main "pe_data" input should be formated as follow:
-#     {
-#         "form_name": form_name,
-#         "manual_meta_information": {"user": user_name}
-#         "entries": {
-#             property_name: value,
-#             property_name: value,
-#             property_name: [value1, value2, ...],
-#             property_name: [
-#                 {
-#                     property_name: value,
-#                     property_name: value,
-#                 }
-#             ],
-#         }
-#     }
-#     """
-#     endpoints = get_endpoints(host)
+def add_process_event_to_dataset(pe_data, study_id, dataset_uuid, host, email=None, password=None):
+    """
+    The main "pe_data" input should be formated as follow:
+    {
+        "form_name": form_name,
+        "manual_meta_information": {"user": user_name}
+        "entries": {
+            property_name: value,
+            property_name: value,
+            property_name: [value1, value2, ...],
+            property_name: [
+                {
+                    property_name: value,
+                    property_name: value,
+                }
+            ]
+        }
+    }
+    """
+    endpoints = get_endpoints(host)
 
-#     prop_name_to_id = map_key_value(endpoints["property"], key="name", value="id")
-#     prop_id_to_name = map_key_value(endpoints["property"], key="id", value="name")
+    # Get authentification token
+    headers = login_and_get_header(
+        login_url = endpoints["login"],
+        use_token = True,
+        email = email,
+        password = password
+    )
 
-#     # Get authentification token
-#     headers = login_and_get_header(endpoints["login"], email, password)
+    # Parse processing event data
+    prop_name_to_id = map_key_value(endpoints["property"], key="name", value="id")
 
-#     # Get study we want to append a dataset to
-#     study_json = api_get(f"{endpoints['study']}/id/{study_id}")
+    pe_converter = FormatConverter(mapper=prop_name_to_id)
+    pe_converter.add_form_format(pe_data["entries"])
+    pe_converter.clean_data()
 
-#     # Generates PE UUID
-#     if not "uuid" in pe_data["entries"]:
-#         pe_data["entries"]["uuid"] = str(uuid.uuid1())
+    # Build API friendly data
+    pe_api_data = {
+        "form_name": pe_data["form_name"],
+        "entries": pe_converter.get_api_format(),
+        "manual_meta_information": pe_data.get("manual_meta_information", {})
+    }
 
-#     # Convert from "form" format to "entries" API friendly format
-#     form_json = get_form_json_by_name(endpoints["form"], pe_data["form_name_construction"])
+    # Post processing event
+    url = f"{endpoints['study']}/id/{study_id}/datasets/id/{dataset_uuid}/pes"
+    pe_res = requests.post(url=url, json=pe_api_data, headers=headers)
 
-#     pe_entryset = EntrySet()
-#     pe_entryset.set_entries_from_simple_format(
-#         data = pe_data["entries"],
-#         form_json = form_json,
-#         map = prop_name_to_id
-#     )
-#     pe_entryset.clean_data()
+    if pe_res.status_code != 201:
+        raise Exception(f"Fail to add processing event to dataset. {pe_res.json()}")
 
-#     # In the next steps, we modify chained references to objects that are actually
-#     # in the study entries at the start. Since we're editing different references
-#     # to the same mutable objects, it works (seems dodgy though)
+    pe_uuid = pe_res.json()["uuid"]
+    print(f"Successfully added processing event to dataset (id: {pe_uuid})")
 
-#     # Recreate study entries and find dataset
-#     study_entryset = EntrySet()
-#     study_entryset.set_entries_from_api_format(study_json["entries"], prop_id_to_name)
-
-#     datasets_entry = study_entryset.get_entry_by_name("datasets")
-#     dataset_entries, dataset_position = datasets_entry.find_nested_formfield_entry("uuid", dataset_uuid)
-#     dataset_entryset = EntrySet(entries=dataset_entries)
-
-#     # Check if "process_events" entry already exists and create if if it doesn't
-#     pe_entry = dataset_entryset.get_entry_by_name("process_events")
-#     if pe_entry is not None:
-#         pe_entry.value.append(pe_entryset.entries)
-#     else:
-#         dataset_entryset.entries.append(StudyEntry(
-#             identifier = prop_name_to_id["process_events"],
-#             name = "process_events",
-#             value = [pe_entryset.entries]
-#         ))
-
-#     datasets_entry.value[dataset_position] = dataset_entryset.entries
-#     study_json["entries"] = study_entryset.api_format()
-#     study_json["form_name"] = pe_data["form_name"]
-#     study_json["manual_meta_information"] = pe_data.get("manual_meta_information", {})
-
-#     # Post study
-#     study_res = requests.put(url=f"{endpoints['study']}/id/{study_id}", json=study_json, headers=headers)
-
-#     if study_res.status_code != 200:
-#         raise Exception(f"Fail to update study. {study_res.json()}")
-
-#     study_info = f"(id: {pe_data['entries']['uuid']})"
-
-#     print(f"Successfully added dataset to study " + study_info)
-
-#     return pe_data['entries']['uuid']
+    return pe_uuid
 
 ###########################################
 ######## Helper functions
