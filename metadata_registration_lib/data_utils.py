b150b3bf380f29518bdddd1b56fa8ed85c6b4f64
Cyril Lopez <cyrlop06@hotmail.com>
2020-11-04 14:40:34 +0100

    Added NormConverter first draft

--- metadata_registration_lib/data_utils.py
+++ metadata_registration_lib/data_utils.py
@@ -1,14 +1,175 @@
-from collections import MutableMapping
+from collections.abc import MutableMapping
+from collections import OrderedDict
+from copy import deepcopy
 
 
+class NormConverter:
+    """
+    Helper class to convert data normalization
+    FORMATS:
+        - nested_data
+            [
+                {
+                    "experiment_id": "EXP_1",
+                    "samples": [
+                        {
+                            "sample_id": "SAM_1",
+                            "treatment": {"treatment_id": "TRE_1"}
+                        },
+                        {
+                            "sample_id": "SAM_2",
+                            "treatment": {"treatment_id": "TRE_2"}
+                        },
+                    ]
+                },
+                {
+                    "experiment_id": "EXP_2",
+                    "samples": [
+                    {
+                        "sample_id": "SAM_3",
+                        "treatment": {"treatment_id": "TRE_3"}
+                    }
+                    ]
+                }
+            ]
+        - flat_data_1
+            [
+                {
+                    "experiment_id": "EXP_1",
+                    "sample_1__sample_id": "SAM_1",
+                    "sample_1__treatment__treatment_id": "TRE_1",
+                    "sample_2__sample_id": "SAM_2",
+                    "sample_2__treatment__treatment_id": "TRE_2",
+                }
+                {
+                    "experiment_id": "EXP_2",
+                    "sample_1__sample_id": "SAM_3",
+                    "sample_1__treatment__treatment_id": "TRE_3",
+                }
+            ]
+        - denorm_data_1
+            [
+                {
+                    "experiment_id": "EXP_1",
+                    "sample__sample_id": "SAM_1",
+                    "sample__treatment__treatment_id": "TRE_1",
+                }
+                {
+                    "experiment_id": "EXP_1",
+                    "sample__sample_id": "SAM_2",
+                    "sample__treatment__treatment_id": "TRE_2",
+                }
+                {
+                    "experiment_id": "EXP_2",
+                    "sample__sample_id": "SAM_3",
+                    "sample__treatment__treatment_id": "TRE_3",
+                }
+            ]
 
-def flatten_dict(d, parent_key="", sep="_"):
+        - denorm_data_2
+            {
+                "expriment_id": ["EXP_1", "EXP_1", "EXP_2"],
+                "sample_id": ["SAM_1", "SAM_2", "SAM_3"],
+                "treatment_id": ["TRE_1", "TRE_2", "TRE_3"],
+            }
+    """
+
+    def __init__(self, nested_data=None, flat_data=None, denorm_data=None):
+        self.nested_data = nested_data
+        self.flat_data = flat_data
+        self.denorm_data = denorm_data
+
+    def get_denorm_data_1_from_nested(
+        self, vars_to_denorm, use_parent_key=True, sep="."
+    ):
+        if self.nested_data is None:
+            raise Exception("'nested_data' needs to be set.")
+
+        if isinstance(self.nested_data, dict):
+            input_data = [self.nested_data]
+        else:
+            input_data = self.nested_data
+
+        self.denorm_data = []
+
+        for d in input_data:
+            # 1. Denormalize
+            for var in vars_to_denorm:
+                tmp_denorm_data = [d]
+                new_denorm_data = []
+                for tmp_d in tmp_denorm_data:
+                    new_denorm_data += denormalize_dict_one_var(tmp_d, var)
+                tmp_denorm_data = new_denorm_data
+            # 2. Flatten
+            for tmp_d in tmp_denorm_data:
+                flat_denorm_d = flatten_dict(
+                    tmp_d, parent_key="", sep=sep, use_parent_key=use_parent_key
+                )
+                self.denorm_data.append(flat_denorm_d)
+
+        return self.denorm_data
+
+    def get_denorm_data_2_from_nested(
+        self, vars_to_denorm, use_parent_key=True, sep=".", missing_value=None
+    ):
+        # 1. Get denormlized flat data
+        denorm_data_1 = self.get_denorm_data_1_from_nested(
+            vars_to_denorm, use_parent_key=use_parent_key, sep=sep
+        )
+
+        # 2. Get all possible keys
+        keys = set()
+        for d in denorm_data_1:
+            keys |= set(d.keys())
+
+        keys = sorted(list(keys))
+
+        # 3. Build final dict of list (of equal length)
+        self.denorm_data = OrderedDict((k, []) for k in keys)
+
+        for d in denorm_data_1:
+            for key in self.denorm_data:
+                self.denorm_data[key].append(d.get(key, missing_value))
+
+        return self.denorm_data
+
+    def get_flat_data_1_from_nested(self):
+        raise NotImplementedError()
+
+    def get_nested_data_from_flat(self):
+        raise NotImplementedError()
+
+    def get_nested_data_from_denorm(self):
+        raise NotImplementedError()
+
+
+def flatten_dict(d, use_parent_key=True, parent_key="", sep="."):
     items = []
     for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
+        new_key = parent_key + sep + k if (parent_key and use_parent_key) else k
         if isinstance(v, MutableMapping):
-            items.extend(flatten_dict(v, new_key, sep=sep).items())
+            items.extend(
+                flatten_dict(
+                    v,
+                    use_parent_key=use_parent_key,
+                    parent_key=new_key,
+                    sep=sep,
+                ).items()
+            )
         else:
             items.append((new_key, v))
     return dict(items)
 
+
+def denormalize_dict_one_var(d, var):
+    """
+    Dupplicate dicts expending a specific list
+    d = {var=[...], ...}
+    """
+    results = []
+    for value in d[var]:
+        sub_d = deepcopy(d)
+        sub_d[var] = value
+        results.append(sub_d)
+
+    return results
--- test/test_data_utils.py
+++ test/test_data_utils.py
@@ -1,10 +1,98 @@
 import unittest
 
-from metadata_registration_lib.data_utils import flatten_dict
+from metadata_registration_lib.data_utils import (
+    flatten_dict,
+    denormalize_dict_one_var,
+    NormConverter,
+)
 
 
 class TestFlatten(unittest.TestCase):
-    def test_flatten_dict(self):
-        d = {"a": 1, "c": {"a": 2, "b": {"x": 5, "y": 10}}, "d": [1, 2, 3]}
-        expected_output = {"a": 1, "c_a": 2, "c_b_x": 5, "d": [1, 2, 3], "c_b_y": 10}
-        self.assertEqual(flatten_dict(d), expected_output)
+    def test_flatten_dict_pk(self):
+        input_data = {"a": 1, "c": {"a": 2, "b": {"x": 5, "y": 10}}, "d": [1, 2, 3]}
+        expected_output = {"a": 1, "c.a": 2, "c.b.x": 5, "d": [1, 2, 3], "c.b.y": 10}
+        output = flatten_dict(input_data, use_parent_key=True, sep=".")
+        self.assertEqual(output, expected_output)
+
+    def test_flatten_dict_no_pk(self):
+        # Unique keys
+        input_data = {"a": 1, "b": {"c": 2, "d": {"e": 5, "f": 10}}, "g": [1, 2, 3]}
+        expected_output = {"a": 1, "c": 2, "e": 5, "f": 10, "g": [1, 2, 3]}
+        output = flatten_dict(input_data, use_parent_key=False)
+        self.assertEqual(output, expected_output)
+
+        # Non unique keys
+        input_data = {"a": 1, "c": {"a": 2, "b": {"x": 5, "y": 10}}, "d": [1, 2, 3]}
+        expected_output = {"a": 2, "d": [1, 2, 3], "x": 5, "y": 10}
+        output = flatten_dict(input_data, use_parent_key=False)
+        self.assertEqual(output, expected_output)
+
+
+class TestDenormalize(unittest.TestCase):
+    def test_denormalize_dict_one_var(self):
+        input_data = {"a": 1, "b": 2, "c": [31, 32], "d": [41, 42]}
+        expected_output = [
+            {"a": 1, "b": 2, "c": 31, "d": [41, 42]},
+            {"a": 1, "b": 2, "c": 32, "d": [41, 42]},
+        ]
+        output = denormalize_dict_one_var(input_data, "c")
+        self.assertEqual(output, expected_output)
+
+
+class TestNormConverter(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.input_data = [
+            {
+                "a": 11,
+                "b": {"c": 21, "d": {"e": 31, "f": 41}},
+                "g": [{"g1": 511, "g2": 611}, {"g1": 512, "g2": 612}],
+            },
+            {
+                "a": 12,
+                "b": {"c": 22, "d": {"e": 32, "f": 42}},
+                "g": [{"g1": 521, "g2": 621}, {"g1": 522, "g2": 622}],
+            },
+        ]
+
+    def test_get_denorm_data_1_from_nested(self):
+        expected_output = [
+            {"a": 11, "b.c": 21, "b.d.e": 31, "b.d.f": 41, "g.g1": 511, "g.g2": 611},
+            {"a": 11, "b.c": 21, "b.d.e": 31, "b.d.f": 41, "g.g1": 512, "g.g2": 612},
+            {"a": 12, "b.c": 22, "b.d.e": 32, "b.d.f": 42, "g.g1": 521, "g.g2": 621},
+            {"a": 12, "b.c": 22, "b.d.e": 32, "b.d.f": 42, "g.g1": 522, "g.g2": 622},
+        ]
+        converter = NormConverter(nested_data=self.input_data)
+        output = converter.get_denorm_data_1_from_nested(
+            vars_to_denorm=["g"], use_parent_key=True
+        )
+        self.assertEqual(output, expected_output)
+
+    def test_get_denorm_data_1_from_nested_no_pk(self):
+        expected_output = [
+            {"a": 11, "c": 21, "e": 31, "f": 41, "g1": 511, "g2": 611},
+            {"a": 11, "c": 21, "e": 31, "f": 41, "g1": 512, "g2": 612},
+            {"a": 12, "c": 22, "e": 32, "f": 42, "g1": 521, "g2": 621},
+            {"a": 12, "c": 22, "e": 32, "f": 42, "g1": 522, "g2": 622},
+        ]
+        converter = NormConverter(nested_data=self.input_data)
+        output = converter.get_denorm_data_1_from_nested(
+            vars_to_denorm=["g"], use_parent_key=False
+        )
+        self.assertEqual(output, expected_output)
+
+    def test_get_denorm_data_2_from_nested(self):
+        expected_output = {
+            "a": [11, 11, 12, 12],
+            "b.c": [21, 21, 22, 22],
+            "b.d.e": [31, 31, 32, 32],
+            "b.d.f": [41, 41, 42, 42],
+            "g.g1": [511, 512, 521, 522],
+            "g.g2": [611, 612, 621, 622],
+        }
+
+        converter = NormConverter(nested_data=self.input_data)
+        output = converter.get_denorm_data_2_from_nested(
+            vars_to_denorm=["g"], use_parent_key=True, missing_value=None
+        )
+        self.assertEqual(output, expected_output)
