6d1e4cc16ae4c55f21ce3c0b318ca02c4396eaf9
Cyril Lopez <cyrlop06@hotmail.com>
2021-12-08 10:19:43 +0100

    Separated reading and writing file utils

--- metadata_registration_lib/file_reading_utils.py
+++ metadata_registration_lib/file_reading_utils.py
@@ -1,64 +1,15 @@
 import csv
 import datetime
-import os
 import re
-import zipfile
-from io import BytesIO
 
 import openpyxl
 import xlrd
-import xlsxwriter
-import xlwt
 
 from metadata_registration_lib.other_utils import fix_float_issue
 
 
-def write_file_from_denorm_data_2(f, data, file_format):
-    """
-    Write a file from the denorm_data_2 format (see fata_utils.py)
-
-    Args:
-        f (file): Input file (open stream)
-        data (dict): data in denorm_data_2 format
-        file_format (str): format of the exported file (xlsx, tsv or csv)
-
-    Raises:
-        Exception: Unsupported file_format
-    """
-    if file_format == "xlsx":
-        wb = xlsxwriter.Workbook(f.name)
-        bold = wb.add_format({"bold": True})
-        ws = wb.add_worksheet()
-
-        col_num = 0
-        for header, data_list in data.items():
-            ws.write(0, col_num, str(header), bold)
-
-            for row_num, value in enumerate(data_list):
-                ws.write(row_num + 1, col_num, str(value))
-
-            col_num += 1
-
-        wb.close()
-
-    elif file_format in ["tsv", "csv"]:
-        delimiters = {"tsv": "\t", "csv": ","}
-        writer = csv.writer(
-            f,
-            delimiter=delimiters[file_format],
-            quotechar='"',
-            quoting=csv.QUOTE_MINIMAL,
-        )
-        writer.writerow(data.keys())
-        for row in zip(*data.values()):
-            writer.writerow(row)
-
-    else:
-        raise Exception(f"Format '{file_format}' not supported")
-
-
 #################################################
-######## Read files as records
+######## Read files as records (specific formats)
 #################################################
 def get_records_and_headers_from_csv(input_file, delimiter=","):
     """
@@ -511,92 +462,3 @@ def remove_empty_rows(rows, empty_values=[None, "", "None"]):
         if not all(v in empty_values for v in row):
             clean_rows.append(row)
     return clean_rows
-
-
-#################################################
-######## Write files
-#################################################
-def write_dict_list_xlsx(file, data, headers):
-    """
-    Write XLSX files
-
-    Args:
-        file (file): Open file stream
-        data (list): Data to write as list of mappings
-        headers (list): list of strings (headers)
-    """
-    wb = xlsxwriter.Workbook(file.name)
-    ws = wb.add_worksheet()
-
-    # Formats
-    f_header = wb.add_format({"bold": True})
-
-    # Write headers
-    for col_num, header in enumerate(headers):
-        ws.write(0, col_num, header, f_header)
-
-    # Write data
-    for row_num, data_dict in enumerate(data, 1):
-
-        for col_num, header in enumerate(headers):
-            ws.write(row_num, col_num, data_dict.get(header, ""))
-
-    # Resize columns
-    for num_col, header in enumerate(headers):
-        width = len(header) * 0.95 if len(header) > 10 else len(header)
-        ws.set_column(num_col, num_col, width + 3)
-
-    wb.close()
-
-
-def write_dict_list_xls(file, data, headers):
-    """
-    Write XLS (old) files
-
-    Args:
-        file (file): Open file stream
-        data (list): Data to write as list of mappings
-        headers (list): list of strings (headers)
-    """
-    wb = xlwt.Workbook()
-    ws = wb.add_sheet("Sheet 1")
-
-    # Formats
-    f_header = xlwt.easyxf("font: bold True;")
-
-    # Write headers
-    for col_num, header in enumerate(headers):
-        ws.write(0, col_num, header, f_header)
-
-    # Write data
-    for row_num, data_dict in enumerate(data, 1):
-
-        for col_num, header in enumerate(headers):
-            ws.write(row_num, col_num, data_dict.get(header, ""))
-
-    # Resize columns
-    for num_col, header in enumerate(headers):
-        width = len(header) * 0.95 if len(header) > 10 else len(header)
-        ws.col(num_col).width = int((width + 3) * 400)
-
-    wb.save(file.name)
-
-
-def get_memory_zip(output_dir, file_names_to_zip):
-    """
-    Creates a ZIP file in memory with the given files in output_dir
-
-    Args:
-        output_dir (tempfile.TemporaryDirectory): Output directory
-        file_names_to_zip (iterable): Relative file names to zip inside output_dir
-
-    Returns:
-        BytesIO: ZIPed file in memory
-    """
-    memory_zip_file = BytesIO()
-    with zipfile.ZipFile(memory_zip_file, "w") as zf:
-        for file_name in file_names_to_zip:
-            os.chdir(output_dir.name)
-            zf.write(file_name, compress_type=zipfile.ZIP_DEFLATED)
-    memory_zip_file.seek(0)
-    return memory_zip_file
