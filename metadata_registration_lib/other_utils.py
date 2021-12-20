09d65c90e464f6804f139ba3d9b5268fdf478c7a
Cyril Lopez <cyrlop06@hotmail.com>
2021-12-20 16:00:34 +0100

    Moved pieces to lab suite tools repo

--- metadata_registration_lib/other_utils.py
+++ metadata_registration_lib/other_utils.py
@@ -1,17 +1,2 @@
-from math import floor, log10
-
-
 def str_to_bool(s):
     return s.lower() in ["true", "1", "y", "yes", "on", "t"]
-
-
-def fix_float_issue(x):
-    """
-    Round to 15 significant numbers
-    See related tests for expected behaviour
-    Trick to round floating points of style 0.009600000000000001 or 0.5599999999999999
-    """
-    # dec = Decimal(str(x)).quantize(Decimal("1.000000000000"))
-    dec = round(x, 15 - int(floor(log10(abs(x)))) - 1)
-
-    return dec
--- test/test_other_utils.py
+++ test/test_other_utils.py
@@ -1,9 +1,6 @@
 import unittest
 
-from metadata_registration_lib.other_utils import (
-    str_to_bool,
-    fix_float_issue,
-)
+from metadata_registration_lib.other_utils import str_to_bool
 
 
 class TestSimpleFunctions(unittest.TestCase):
@@ -13,16 +10,3 @@ class TestSimpleFunctions(unittest.TestCase):
 
         for s in ["n", "0", "None", "off", "f", ""]:
             assert str_to_bool(s) == False
-
-    def test_fix_float_issue(self):
-        # Floats with issues
-        assert fix_float_issue(0.009600000000000001) == 0.0096
-        assert fix_float_issue(0.5599999999999999) == 0.56
-        assert fix_float_issue(0.1 + 0.2) == 0.3
-        assert fix_float_issue(1000000000.1 + 1000000000.2) == 2000000000.3
-        assert fix_float_issue(10000000000.1 + 10000000000.2) == 20000000000.3
-        assert fix_float_issue(100000000000.1 + 100000000000.2) == 200000000000.3
-        assert fix_float_issue(1000000000000.1 + 1000000000000.2) == 2000000000000.3
-        # Floats without issues
-        assert fix_float_issue(0.1234) == 0.1234
-        assert fix_float_issue(123456.7) == 123456.7
